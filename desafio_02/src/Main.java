dfab654786718409f1516584147b2d8c19f67639
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-03-30 13:50:26 -0300

    Modificando alguns desafios

--- desafio_02/src/Main.java
+++ desafio_02/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
-        JSONRead json = new JSONRead();
-        json.convertJson();
+    public static void main(String[] args){
+
+
+
+
 
     }
 }
